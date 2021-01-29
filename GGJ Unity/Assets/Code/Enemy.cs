b75644bb8b2671f4d8cb6475ad0f9e1c73b8fa52
DESKTOP-AVTI26A\burto <matthewburton@webster.edu>
2021-01-29 13:03:20 -0600

    Initial Commit. Made Basic Slingshot

---
+++ GGJ Unity/Assets/Code/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour 
+{
+    private bool _isHit = false;
+
+    public void Init(Vector2 direction, float speed)
+    {
+        GetComponent<Rigidbody2D>().AddForce(direction * speed, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(!_isHit)
+        {
+            EnemyDispenserController.Instance.HandleEnemyDestroyed(this);
+            _isHit = true;
+            GetComponent<Rigidbody2D>().gravityScale = 1;
+        }
+    }
+}
---
+++ GGJ Unity/Assets/Code/EnemyDispenserController.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDispenserController : MonoBehaviour 
+{
+    public static EnemyDispenserController Instance;
+
+    [SerializeField] private EnemyDispenser topLeftDispenser;
+    [SerializeField] private EnemyDispenser topRightDispenser;
+    [SerializeField] private EnemyDispenser bottomLeftDispenser;
+    [SerializeField] private EnemyDispenser bottomLeft2Dispenser;
+    [SerializeField] private EnemyDispenser bottomRightDispenser;
+    [SerializeField] private EnemyDispenser bottomRight2Dispenser;
+
+    public float speed;
+
+    [HideInInspector] public List<Enemy> activeEnemies;
+
+    void Awake () 
+    {
+        Instance = this;
+
+		topLeftDispenser.Init(this);
+		bottomLeft2Dispenser.Init(this);
+		topRightDispenser.Init(this);
+		bottomRight2Dispenser.Init(this);
+        bottomLeftDispenser.Init(this);
+        bottomRightDispenser.Init(this);
+    }
+
+    private void Start()
+    {
+        DetermineIfShouldFire();
+    }
+
+    void DetermineIfShouldFire()
+    {
+        if(activeEnemies.Count <= 0)
+        {
+            FireRandomSequence();
+        }
+    }
+
+    void FireRandomSequence()
+    {
+		var randomInt = Random.Range (0, 5);
+
+		if (randomInt == 0)
+		{
+			FireFour();
+		}
+		else if (randomInt == 1)
+		{
+			FireTwoLeft();
+		}
+		else if (randomInt == 2)
+		{
+			FireTwoRight();
+		}
+		else if (randomInt == 3)
+		{
+			FireMix1();
+		}
+		else if (randomInt == 4)
+		{
+			FireMix2();
+		}
+    }
+
+    public void FireFour()
+    {
+        activeEnemies.Add(topLeftDispenser.FireEnemy());
+        activeEnemies.Add(topRightDispenser.FireEnemy());
+        activeEnemies.Add(bottomLeftDispenser.FireEnemy());
+        activeEnemies.Add(bottomRightDispenser.FireEnemy());
+    }
+
+	public void FireTwoLeft()
+	{
+        activeEnemies.Add(topLeftDispenser.FireEnemy());
+        activeEnemies.Add(bottomLeft2Dispenser.FireEnemy());
+	}
+
+	public void FireTwoRight()
+	{
+        activeEnemies.Add(topRightDispenser.FireEnemy());
+        activeEnemies.Add(bottomRight2Dispenser.FireEnemy());
+	}
+
+	public void FireMix1()
+	{
+		activeEnemies.Add(bottomLeft2Dispenser.FireEnemy());
+		activeEnemies.Add(topRightDispenser.FireEnemy());
+	}
+
+	public void FireMix2()
+	{
+		activeEnemies.Add(bottomRight2Dispenser.FireEnemy());
+		activeEnemies.Add(topLeftDispenser.FireEnemy());
+	}
+
+    public void HandleEnemyDestroyed(Enemy destroyedEnemy)
+    {
+        StartCoroutine(HandleEnemyDestoyedCo(destroyedEnemy));
+    }
+
+    private IEnumerator HandleEnemyDestoyedCo(Enemy destroyedEnemy)
+    {
+        yield return new WaitForSeconds(2);
+        activeEnemies.Remove(destroyedEnemy);
+        Destroy(destroyedEnemy.gameObject);
+        DetermineIfShouldFire();
+    }
+}
