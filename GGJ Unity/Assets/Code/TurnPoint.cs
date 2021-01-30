64a9cf0cde61f1c4362a338e5f1c05e18c5f806f
WEBSTER UNIVERSITY <DTS@WEBSTER.EDU>
2021-01-29 19:31:03 -0600

    added zombie stuff

---
+++ GGJ Unity/Assets/Code/TurnPoint.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnPoint : MonoBehaviour
+{
+    public enum Direction
+    {
+        down,
+        left,
+        right
+    }
+}
---
+++ GGJ Unity/Assets/Code/Zombie.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+    public float walkSpeed;
+    public int xDir;
+    public int yDir;
+    public float initialHealth;
+    public float currentHealth;
+
+    private void Update()
+    {
+        Walk();
+    }
+
+    public void Walk()
+    {
+
+    }
+
+    public void Splode()
+    {
+
+    }
+
+}
