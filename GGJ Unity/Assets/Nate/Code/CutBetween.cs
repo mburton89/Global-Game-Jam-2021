baa816981ff7398cf28d512cf1fbad8a03e5d530
WEBSTER UNIVERSITY <DTS@WEBSTER.EDU>
2021-01-30 18:51:29 -0600

    things

---
+++ GGJ Unity/Assets/Nate/Code/CutBetween.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutBetween : MonoBehaviour
+{
+    private Character _target;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+
+        if (collision.GetComponent<Character>())
+        {
+            _target = collision.GetComponent<Character>();
+            _target.movementSpeed = _target.movementSpeed / 4;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Character>())
+        {
+            _target.movementSpeed = _target.movementSpeed * 4;
+        }
+    }
+}
