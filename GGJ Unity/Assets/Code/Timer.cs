677a27dff162ad584858afea92879c4867c6a7fc
DESKTOP-AVTI26A\burto <matthewburton@webster.edu>
2021-01-30 23:46:21 -0600

    prefabs scene

---
+++ GGJ Unity/Assets/Code/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public float timeLeft;
+    public TextMeshProUGUI timeText;
+    private const string TIME_PREFIX = "00:";
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        string timeString = timeLeft.ToString().Substring(0, 2);
+        timeText.SetText(TIME_PREFIX + timeString);
+        if (timeLeft < 0)
+        {
+            //GameOver();
+        }
+    }
+}
