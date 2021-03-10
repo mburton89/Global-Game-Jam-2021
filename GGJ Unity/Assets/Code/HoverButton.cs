2f763cebede3d20524de930bf2b9e95a37e5619e
DESKTOP-AVTI26A\burto <matthewburton@webster.edu>
2021-03-10 15:04:46 -0600

    Updated Intro, menu, and gameplay scene for veritcal mobile. Removed unused sprites

--- GGJ Unity/Assets/Code/HoverButton.cs
+++ GGJ Unity/Assets/Code/HoverButton.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class HoverButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+public class HoverButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler, IPointerDownHandler
 {
     public GameObject HoverEffect;
     public bool isStartButton;
+
+    void Awake()
+    {
+        GetComponent<Image>().color = Color.clear;
+    }
 
     public void OnPointerClick(PointerEventData eventData)
     {
@@ -14,6 +20,11 @@ public class HoverButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
         {
             MainMenu.Instance.Play();
         }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        HoverEffect.SetActive(true);
     }
 
     public void OnPointerEnter(PointerEventData eventData)
---
+++ GGJ Unity/Assets/Code/Mobile/CameraManagerForMobile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManagerForMobile : MonoBehaviour
+{
+    //camera size for 1:2 (0.5) == 10.3
+    //camera size for 3:4 (0.75) == 6.9
+
+    void Start()
+    {
+        float aspectRatio = (float)Screen.width / (float)Screen.height;
+        float cameraSize;
+        cameraSize = (1.25f - aspectRatio) * 13.733f;
+        Camera.main.orthographicSize = cameraSize;
+    }
+}
