99d8b648f5e75e691b16dffa1c9b17955b4ff889
DESKTOP-AVTI26A\burto <matthewburton@webster.edu>
2022-02-03 22:32:42 -0600

    Intro Projector

---
+++ GGJ Unity/Assets/Code/LightFlicker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightFlicker : MonoBehaviour
+{
+    private Image _light;
+    private float _secondsOn;
+    private float _secondsOff;
+
+    private void Awake()
+    {
+        _light = GetComponent<Image>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(Flicker());
+    }
+
+    private IEnumerator Flicker()
+    {
+        _secondsOff = Random.Range(0.025f, 0.1f);
+        _secondsOn = Random.Range(0.025f, 0.1f);
+
+        _light.color = Color.white;
+        yield return new WaitForSeconds(_secondsOn);
+
+        _light.color = new Color(1, 1, 1, Random.Range(0.85f, 1f));
+        yield return new WaitForSeconds(_secondsOff);
+
+        StartCoroutine(Flicker());
+    }
+}
---
+++ GGJ Unity/Assets/Code/Projector.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
+using TMPro;
+
+public class Projector : MonoBehaviour
+{
+    [SerializeField] Transform projectorPivot;
+    [SerializeField] TextMeshProUGUI captionText;
+    [SerializeField] Button nextSlideButton;
+    [SerializeField] Button skipButton;
+    [SerializeField] AudioSource nextSlideAudioSource;
+    [SerializeField] List<AudioClip> nextSlideClips;
+    [SerializeField] List<AudioSource> slideClips;
+    [SerializeField] List<Image> slides;
+    [SerializeField] List<string> captions;
+    [SerializeField] float secondsToSwitchSlides;
+    [SerializeField] Ease ease;
+    [SerializeField] Transform lightFlicker;
+    int currentSlideIndex;
+    bool canGoToNextSlide;
+
+    private void Awake()
+    {
+        canGoToNextSlide = true;
+    }
+
+    void OnEnable()
+    {
+        nextSlideButton.onClick.AddListener(GoToNextSlide);
+        skipButton.onClick.AddListener(Skip);
+    }
+
+    void OnDisable()
+    {
+        nextSlideButton.onClick.RemoveListener(GoToNextSlide);
+        skipButton.onClick.RemoveListener(Skip);
+    }
+
+    void GoToNextSlide()
+    {
+        if (currentSlideIndex < slides.Count - 1 && canGoToNextSlide)
+        {
+            StartCoroutine(nameof(GoToNextSlideCo));
+        }
+        else
+        {
+            Skip();
+        }
+    }
+
+    private IEnumerator GoToNextSlideCo()
+    {
+        canGoToNextSlide = false;
+        lightFlicker.localScale = Vector3.zero;
+        nextSlideAudioSource.clip = nextSlideClips[Random.Range(0, nextSlideClips.Count - 1)];
+        nextSlideAudioSource.Play();
+        slides[currentSlideIndex].DOFade(0.33f, .1f);
+        projectorPivot.DORotate(new Vector3(0, 0, projectorPivot.eulerAngles.z - 30), secondsToSwitchSlides, RotateMode.Fast).SetEase(ease);
+        yield return new WaitForSeconds(secondsToSwitchSlides);
+        currentSlideIndex++;
+        lightFlicker.localScale = Vector3.one;
+        slides[currentSlideIndex].DOFade(1f, .1f);
+        captionText.SetText(captions[currentSlideIndex]);
+        slideClips[currentSlideIndex].Play();
+        yield return new WaitForSeconds(.1f);
+        canGoToNextSlide = true;
+    }
+
+    void Skip()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
